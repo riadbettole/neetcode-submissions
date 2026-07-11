77d970e11d915ca8237bc15be0afc3e1a1996f10
Riad Bettole <118698087+riadbettole@users.noreply.github.com>
2026-07-11 20:51:58 +0100

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visited = set()
+        courses = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            courses[crs].append(pre)
+
+        def dfs(node):
+            if node in visited:
+                return False
+            if courses[node] == []:
+                return True
+
+            visited.add(node)
+                             
+            for neighbor in courses[node]:
+                if not dfs(neighbor): return False
+                
+            visited.remove(node) 
+
+            courses[node] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
