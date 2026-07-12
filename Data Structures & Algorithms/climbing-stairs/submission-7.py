e0e5391984754b4b14dcb2930e14fd6507559d8b
Riad Bettole <118698087+riadbettole@users.noreply.github.com>
2026-07-12 15:10:16 +0100

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        f, f1 = 1, 1
+        for i in range(1, n):
+            f, f1 = f1, f + f1
+        return f1 
+        # cache = {}
+        # def dfs(i):
+        #     if i >= n:
+        #         return i == n
+        #     if i in cache:
+        #         return cache[i]
+        #     cache[i] = dfs(i + 1) + dfs(i + 2) 
+        #     return cache[i]
+        # return dfs(0)
