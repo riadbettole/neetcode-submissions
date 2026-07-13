f6369da432adbcbf90c61cba601f73e175ab3f98
Riad Bettole <118698087+riadbettole@users.noreply.github.com>
2026-07-13 22:29:50 +0100

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        cache = {}
+        def dfs(i):
+            if  i >= len(nums):
+                return 0
+            if i in cache:
+                return cache[i]
+            longest = 1
+            for j in range(i + 1, len(nums)):
+                if nums[j] > nums[i]:
+                    longest = max(longest, 1 + dfs(j))
+            cache[i] = longest
+            return cache[i]
+
+        res = 0
+        for i in range(len(nums)):
+            res = max(res, dfs(i))
+
+        return res
